45e5513c9f3ebe0583140b5113592deca85e8cf2
Andreas Göbel <109575712+andreas-goebel@users.noreply.github.com>
2023-01-18 12:13:26 +0100

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -5,7 +5,7 @@ window.onload = function() {
   const urlParams = new URLSearchParams(queryString);
   var yaml = urlParams.get('yaml')
   if (!yaml) { yaml = queryString.substring(1) }
-  if (!yaml) { yaml = 'https://raw.githubusercontent.com/eliona-smart-building-assistant/eliona-api/master/openapi.yaml' }
+  if (!yaml) { yaml = 'https://raw.githubusercontent.com/eliona-smart-building-assistant/eliona-api/main/openapi.yaml' }
   
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container  
   window.ui = SwaggerUIBundle({
